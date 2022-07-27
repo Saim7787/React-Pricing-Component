92b80f30cb8c66195dbbe3742fb1f3c96cd77540
Thomas Sankara <thomassankara74@gmail.com>
2022-07-27 16:17:35 +0300

    add background images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { useState } from "react"
+import bgBottom from "./images/bg-bottom.svg"
+import bgTop from "./images/bg-top.svg"
 
 function App() {
   const [annually, setAnnually] = useState(false)
 
   return (
     <>
+      <div className="absolute top-0 right-0 -z-10">
+        <img src={bgTop} alt="" className="w-full max-w-full" />
+      </div>
+
       <section className="p-5 lg:flex lg:flex-col items-center justify-center lg:h-screen">
         <h1 className="text-center text-slate-700 mb-8 text-4xl">
           Our Pricing
@@ -129,6 +135,10 @@ function App() {
           .
         </div>
       </section>
+
+      <div className="absolute left-0 bottom-0 -z-10">
+        <img src={bgBottom} alt="" className="w-full max-w-full" />
+      </div>
     </>
   )
 }
